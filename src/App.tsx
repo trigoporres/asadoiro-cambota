b9d44a180cdf5b5bec36b0ea4b411dc5f5d564c5
santiago.trigo <santiago.trigo@devo.com>
2023-10-02 09:04:25 +0200

    add genesys-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react'
+import { Toolbar  } from '@devoinc/genesys-ui';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  <React.Fragment key=".0">
+    <Toolbar.ToolbarGroup>
+      <Toolbar.ToolbarItem>
+        Item One - G1
+      </Toolbar.ToolbarItem>
+      <Toolbar.ToolbarItem>
+        Item Two - G1
+      </Toolbar.ToolbarItem>
+    </Toolbar.ToolbarGroup>
+    <Toolbar.ToolbarGroup>
+      <Toolbar.ToolbarSeparator />
+    </Toolbar.ToolbarGroup>
+    <Toolbar.ToolbarGroup>
+      <Toolbar.ToolbarItem>
+        Item One - G2
+      </Toolbar.ToolbarItem>
+    </Toolbar.ToolbarGroup>
+    <Toolbar.ToolbarGroup>
+      <Toolbar.ToolbarItem>
+        Item One - G3
+      </Toolbar.ToolbarItem>
+    </Toolbar.ToolbarGroup>
+  </React.Fragment>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App.tsx'
+import '@devoinc/genesys-base-styles/dist/styles.css';
+
+import { ThemeProvider } from 'styled-components';
+import { light } from '@devoinc/genesys-brand-devo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={light}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
